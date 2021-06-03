6df6bdb0864bbaea20cf3ec02212b4d831f1c28d
Feriow <feriow@gmail.com>
2021-06-03 18:30:10 -0300

    small change

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -7,7 +7,7 @@ import Stock from '../shared/models/stock-model';
 })
 export class DashboardService {
   readonly baseUrl =
-    'http://bootcamp-dio-santander-week.herokuapp.com/bootcamp';
+    'https://bootcamp-dio-santander-week.herokuapp.com/bootcamp';
 
   constructor(private http: HttpClient) {}
 
